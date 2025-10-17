0a986239fe7378c5397285db871a21717ac8a2aa
Mikle Shmatko <shmatko.mixail@mail.ru>
2025-10-17 17:59:08 +0300

    Декоратор logger(func)

---
+++ prakt2/1.py
@@ -0,0 +1,18 @@
+import functools
+
+def logger(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f"Вызов функции {func.__name__} с аргументами {args} и {kwargs}")
+        result = func(*args, **kwargs)
+        print(f"Функция {func.__name__} вернула {result}")
+        return result
+    return wrapper
+
+@logger
+def add(a, b):
+    return a + b
+
+@logger
+def greet(name, greeting="Hello"):
+    return f"{greeting}, {name}!"
